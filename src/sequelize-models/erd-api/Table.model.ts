a91d2d23d79f138d46adbfa1e996912abd682f81
a7591 <a7591@zeroweb.kr>
2023-11-09 16:40:48 +0900

    added position and type column to Table.model.ts

--- src/sequelize-models/erd-api/Table.model.ts
+++ src/sequelize-models/erd-api/Table.model.ts
@@ -14,12 +14,21 @@ import {Erd, IErd} from "./Erd.model";
 import {IColumn, Column as ColumnModel} from "./Column.model";
 import {Relation} from "./Relation.model";
 
+export interface INodePosition {
+  x: number,
+  string: number
+}
+
+
 export interface ITable {
   id: string;
   createdAt: string;
   updatedAt: string;
+
   name: string;
   color: string;
+  position: INodePosition;
+  type: string;
 
   // Foreign key
   erdId: string;
@@ -29,7 +38,8 @@ export interface ITable {
   columns?: IColumn[]
 }
 
-export interface ICTable extends Optional<ITable, 'id' | 'createdAt' | 'updatedAt'> {
+export interface ICTable extends Omit<Optional<ITable, 'id' | 'createdAt' | 'updatedAt'>, 'position'> {
+  position: string
 }
 
 @SequelizeTable({
@@ -54,6 +64,23 @@ export class Table extends Model<ITable, ICTable> {
   })
   declare name: string
 
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false
+  })
+  declare type: string
+
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    get() {
+      return JSON.parse(this.getDataValue("position"))
+    }
+  })
+  declare position: string
+
   // ForeignKey
   @ForeignKey(() => Erd)
   @Column({
@@ -68,7 +95,7 @@ export class Table extends Model<ITable, ICTable> {
 
   @HasMany(() => ColumnModel, {
     onUpdate: "CASCADE",
-    onDelete: "CASCADE"
+    onDelete: "CASCADE",
   })
   declare columns: ColumnModel[]
 
@@ -77,4 +104,13 @@ export class Table extends Model<ITable, ICTable> {
     onDelete: "CASCADE"
   })
   declare relations: Relation[]
+
+  @Column(DataType.VIRTUAL)
+  get data () {
+    return {
+      name: this.getDataValue('name'),
+      color: this.getDataValue('color'),
+      columns: this.getDataValue('columns' as any),
+    }
+  }
 }
