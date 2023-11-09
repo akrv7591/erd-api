294d53af3bef6275dd523c3c7a32d6480b46f855
a7591 <a7591@zeroweb.kr>
2023-11-09 16:44:48 +0900

    added list api for table

---
+++ src/routes/v1/erd-router/table-router/list.ts
@@ -0,0 +1,25 @@
+import {RequestHandler} from "express";
+import {errorHandler} from "../../../../middleware/errorHandler";
+import {Table} from "../../../../sequelize-models/erd-api/Table.model";
+import {Column} from "../../../../sequelize-models/erd-api/Column.model";
+import {Relation} from "../../../../sequelize-models/erd-api/Relation.model";
+import {erdSequelize} from "../../../../sequelize-models/erd-api";
+
+export const tableList: RequestHandler = async (req, res) => {
+  try {
+    const data = await Table.findAndCountAll({
+      where: {
+        erdId: req.params['erdId']
+      },
+      include: [Column, Relation],
+      order: [
+        [erdSequelize.col('order'), 'ASC']
+      ]
+    })
+
+    res.json(data)
+
+  } catch (e: any) {
+    errorHandler(e, req, res)
+  }
+}
