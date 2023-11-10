87e9a1f95edc5c083b06b5b1ab8dbaa4303b2131
a7591 <a7591@zeroweb.kr>
2023-11-10 16:16:22 +0900

    UserTeam removed id and added composite primary key

--- src/sequelize-models/erd-api/UserTeam.model.ts
+++ src/sequelize-models/erd-api/UserTeam.model.ts
@@ -1,25 +1,23 @@
 import {Optional} from "sequelize";
 import {Column, DataType, ForeignKey, Model, PrimaryKey, Table} from "sequelize-typescript";
-import {createId} from "@paralleldrive/cuid2";
 import {IUser, User} from "./User.model";
 import {ITeam, Team} from "./Team.model";
 
 export interface IUserTeam {
-  id: string
+  //Composite primary keys
+  userId: string
+  teamId: string
+
   createdAt: Date
   updatedAt: Date
   isAdmin: boolean
-
-  //Foreign keys
-  userId: string
-  teamId: string
 
   //Relations
   user?: IUser
   team?: ITeam
 }
 
-export interface ICUserTeam extends Optional<IUserTeam, 'id' | 'createdAt' | 'updatedAt'> {
+export interface ICUserTeam extends Optional<IUserTeam, 'createdAt' | 'updatedAt'> {
 }
 
 @Table({
@@ -28,14 +26,22 @@ export interface ICUserTeam extends Optional<IUserTeam, 'id' | 'createdAt' | 'up
   timestamps: true,
 })
 export class UserTeam extends Model<IUserTeam, ICUserTeam> {
+  //Composite primary keys
   @PrimaryKey
+  @ForeignKey(() => User)
   @Column({
     type: DataType.STRING,
-    allowNull: false,
-    unique: true,
-    defaultValue: () => createId()
+    allowNull: false
   })
-  declare id: string;
+  declare userId: string
+
+  @PrimaryKey
+  @ForeignKey(() => Team)
+  @Column({
+    type: DataType.STRING,
+    allowNull: false
+  })
+  declare teamId: string
 
   @Column({
     type: DataType.BOOLEAN,
@@ -44,20 +50,7 @@ export class UserTeam extends Model<IUserTeam, ICUserTeam> {
   })
   declare isAdmin: boolean
 
-  //Foreign keys
-  @ForeignKey(() => User)
-  @Column({
-    type: DataType.STRING,
-    allowNull: false
-  })
-  declare userId: string
 
-  @ForeignKey(() => Team)
-  @Column({
-    type: DataType.STRING,
-    allowNull: false
-  })
-  declare teamId: string
 
   // Relations
   // @BelongsTo(() => User)
