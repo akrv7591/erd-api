da98eb667b87d54d2a08f335121ec421be7c01b8
a7591 <a7591@zeroweb.kr>
2024-03-14 10:27:51 +0900

    added entity router

---
+++ src/routes/v1/erd-router/entity/index.ts
@@ -0,0 +1,5 @@
+import express from "express";
+
+const entityRouter = express.Router()
+
+export default entityRouter
--- src/routes/v1/erd-router/index.ts
+++ src/routes/v1/erd-router/index.ts
@@ -6,9 +6,11 @@ import {validateSchemas} from "../../../middleware/validateSchemas";
 import {erdSchema} from "../../../validations/erd.validation";
 import {deleteErd} from "./deleteErd";
 import {detail} from "./detail";
+import entityRouter from "./entity";
 
 const erdRouter = express.Router()
 
+erdRouter.use("/:erdId/entity", entityRouter)
 erdRouter.delete("/:erdId", deleteErd)
 erdRouter.get("/:erdId", detail)
 erdRouter.get("", pagination({searchFields: ['name'], like: true}), list)
