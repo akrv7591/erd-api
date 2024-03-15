2b536b6da30253bb367cc7c866edb5badc7d3348
a7591 <a7591@zeroweb.kr>
2024-03-15 10:45:05 +0900

    updated memo patch function

--- src/socket/multiplayer/memo-controller.ts
+++ src/socket/multiplayer/memo-controller.ts
@@ -1,9 +1,10 @@
 import {CallbackDataStatus, Key, MemoEnum} from "../../enums/multiplayer";
 import {Server, Socket} from "socket.io";
 import {RedisClientType} from "redis";
-import {ICMemo, IMemo, Memo} from "../../sequelize-models/erd-api/Memo.mode";
+import {IMemo, Memo} from "../../sequelize-models/erd-api/Memo.mode";
 import {Transaction} from "sequelize";
 import {erdSequelize} from "../../sequelize-models/erd-api";
+import {INodePosition} from "../../sequelize-models/erd-api/Entity.model";
 
 export interface CallbackDataType {
   type: MemoEnum;
@@ -24,19 +25,28 @@ function getCallbackData(type: MemoEnum): CallbackDataType {
 
 export const memoController = (io: Server, socket: Socket, redis: RedisClientType) => {
   const playgroundId = socket.handshake.auth['playgroundId']
+  const playerId = socket.handshake.auth['playerId']
   const playgroundKey = `${Key.playground}:${playgroundId}`
 
-  const onAdd = async (data: ICMemo, callback: Function) => {
+  const onAdd = async (m: {position: INodePosition, data: Pick<IMemo, 'color' | 'content'>}, callback: Function) => {
     const callbackData = getCallbackData(MemoEnum.add)
     let transaction: Transaction | null = null
 
     try {
       transaction = await erdSequelize.transaction()
-      const memo = await Memo.create(data, {transaction})
+      const memo = await Memo.create({
+        position: m.position,
+        ...m.data,
+        erdId: playgroundId,
+        userId: playerId
+        }, {transaction})
+      await memo.reload({transaction})
       await redis.json.arrAppend(playgroundKey, '.memos', memo.toJSON() as any)
 
       callbackData.status = CallbackDataStatus.OK
-      callbackData.data = data
+      callbackData.data = {
+        ...memo.toJSON(),
+      }
       await transaction.commit()
       socket.to(playgroundKey).emit(MemoEnum.add, memo.toJSON())
       callback(callbackData)
@@ -49,47 +59,49 @@ export const memoController = (io: Server, socket: Socket, redis: RedisClientTyp
 
   const onPut = async (data: IMemo, callback: Function) => {
     const callbackData = getCallbackData(MemoEnum.put)
-    let transaction: Transaction | null = null
-
     try {
-      transaction = await erdSequelize.transaction()
-      await Memo.upsert(data, {transaction})
-      await redis.json.set(playgroundKey, `.memos[?(@.id=='${data.id}')]`, data as any)
+      const memoRedisData: any[] = []
+      for (const [key, value] of Object.entries(data)) {
+        if (key === 'id') continue
+        memoRedisData.push({ key: playgroundKey, path: `.memos[?(@.id=='${data.id}')].${key}`, value });
+      }
+
+      await Promise.all([
+        redis.json.mSet(memoRedisData),
+        Memo.update(data, {where: {id: data.id}})
+      ])
 
       callbackData.status = CallbackDataStatus.OK
       callbackData.data = data
 
-      await transaction.commit()
       socket.to(playgroundKey).emit(MemoEnum.put, data)
       callback(callbackData)
 
 
     } catch (e) {
       console.error("MEMO PUT ERROR", e)
-      await transaction?.rollback()
       callback(callbackData)
     }
   }
 
 
-  const onPatch = async ({memoId, key, value}: { memoId: string, key: string, value: any }, callback: Function) => {
+  const onPatch = async (data: { memoId: string, key: string, value: any }, callback: Function) => {
     const callbackData = getCallbackData(MemoEnum.patch)
     let transaction: Transaction | null = null
-
     try {
       transaction = await erdSequelize.transaction()
-      await Memo.update({[key]: value}, {
+      await Memo.update({[data.key]: data.value}, {
         where: {
-          id: memoId
+          id: data.memoId
         },
         transaction
       })
-      await redis.json.set(playgroundKey, `.memos[?(@.id=='${memoId}')].${key}`, value)
+      await redis.json.set(playgroundKey, `.memos[?(@.id=='${data.memoId}')].data.${data.key}`, data.value)
 
       callbackData.status = CallbackDataStatus.OK
-      callbackData.data[key] = value
+      callbackData.data = data
       await transaction.commit()
-      socket.to(playgroundKey).emit(MemoEnum.patch, {memoId, key, value})
+      socket.to(playgroundKey).emit(MemoEnum.patch, data)
       callback(callbackData)
 
     } catch (e) {
