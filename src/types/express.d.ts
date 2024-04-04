52c1230609d4c0778978463366b98b217d91b5e5
a7591 <a7591@zeroweb.kr>
2024-04-04 09:31:51 +0900

    update IAuthorizedUser type

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -2,11 +2,6 @@ import {FindOptions} from "sequelize/types/model";
 import {JWTPayload} from "jose";
 interface IAuthorizedUser extends JWTPayload {
   id: string
-  name: string
-  email: string
-  emailVerified: string
-  createdAt: string
-  updatedAt: string
 }
 
 declare global {
