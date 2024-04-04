ab0c029b79770028b6c05a0f111f23e229ce4f16
a7591 <a7591@zeroweb.kr>
2024-04-04 09:31:10 +0900

    updated pagination middleware

--- src/middleware/pagination.ts
+++ src/middleware/pagination.ts
@@ -1,16 +1,27 @@
-import {Request, Response, NextFunction} from "express";
+import {NextFunction, Request, Response} from "express";
 import {Op, Order} from "sequelize";
 import httpStatus from "http-status";
 import {errorHandler} from "../utils/errorHandler";
 import {COMMON} from "../constants/common";
+import {HttpStatusCode} from "axios";
+import {FindOptions} from "sequelize/types/model";
+
 interface IPaginationOptions {
   searchFields?: string[],
   like?: boolean
 }
+export type PaginationRequestQuery = {
+  q?: string
+  limit?: string
+  offset?: string
+  order?: FindOptions['order']
+}
 
-export const pagination = ({searchFields, like}: IPaginationOptions) =>
+export type PaginationRequest = Request<any, {}, {}, PaginationRequestQuery>
+
+export const pagination = ({searchFields, like = true}: IPaginationOptions) =>
   async (
-    req: Request,
+    req: PaginationRequest,
     res: Response,
     next: NextFunction
   ) => {
@@ -20,11 +31,20 @@ export const pagination = ({searchFields, like}: IPaginationOptions) =>
       q,
       limit = 10,
       offset = 0,
-      order = ["createdAt", 'desc'],
+      order = ['createdAt', 'DESC'],
     } = req.query;
 
     // Check if the query string is valid and search fields exist
     if (q && searchFields && searchFields.length > 0) {
+      if (!Array.isArray(searchFields)) {
+        return errorHandler(req, res, HttpStatusCode.InternalServerError, COMMON.API_ERRORS.INTERNAL_SERVER_ERROR)
+      }
+
+      if (like !== undefined) {
+        if (typeof like !== 'boolean') {
+          return errorHandler(req, res, HttpStatusCode.InternalServerError, COMMON.API_ERRORS.INTERNAL_SERVER_ERROR)
+        }
+      }
       const query = like? `%${q}%`: q
       pagination.where = {
         [Op.or]:  searchFields.map(field => ({[field]: { [Op.like]: query}}))
@@ -32,13 +52,18 @@ export const pagination = ({searchFields, like}: IPaginationOptions) =>
     }
 
     // Check if the order is valid
-    if (Array.isArray(order) && order.length > 0) {
-      pagination.order = order as Order
+    if (order !== undefined) {
+      if (!Array.isArray(order)) {
+        return errorHandler(req, res, HttpStatusCode.InternalServerError, COMMON.API_ERRORS.INTERNAL_SERVER_ERROR)
+      } else if (order.length > 0) {
+        pagination.order = order as Order
+      }
     }
 
     // Check if the limit and offset are valid
-    limit = parseInt(String(limit))
-    offset = parseInt(String(offset))
+    limit = Number(Number(limit).toFixed())
+    offset = Number(Number(offset).toFixed())
+
 
     if (isNaN(limit)) {
       return errorHandler(req, res, httpStatus.BAD_REQUEST, COMMON.API_ERRORS.LIMIT_IS_NOT_VALID)
