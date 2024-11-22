2e4268448fa5e1665e81e6372a2facac99c21549
a7591 <a7591@zeroweb.kr>
2024-11-22 11:20:53 +0900

    - Updated Dockerfile and ecosystem.config.js

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,10 @@ RUN yarn build
 
 FROM keymetrics/pm2:latest-alpine
 
-COPY --from=builder . .
+COPY --from=builder /app/dist ./dist
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/package.json ./package.json
+
 EXPOSE 3002
 
 CMD [ "pm2-runtime", "start", "ecosystem.config.js"]
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  apps : [{
-    name: "erdiagramly",
-    script: './dist/index.js',
-    instances: "4",
-  }],
+  apps: [
+    {
+      name: "erdiagramly",
+      script: "./dist/index.js",
+      instances: "4",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
